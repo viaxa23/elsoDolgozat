cfe4ccb9b781c129ee5f1827076b38737a1ab5e1
Visnyei Ádám <visnyei.adam@sob.lidice.local>
2020-09-24 09:14:06 +0200

    első kommit

---
+++ javascript.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const diakok = [
+    {'nev': 'Tóth Balázs', 'osztaly': '14SZF'},
+    {'nev': 'Börcsök Dávid', 'osztaly': '14SZF'},
+    {'nev': 'Szabó Levente', 'osztaly': '14SZF'},
+    {'nev': 'Pásztor Krisztián', 'osztaly': '14SZF'},
+    {'nev': 'Kucsera Kinga', 'osztaly': '14SZF'},
+    {'nev': 'Visnyei Ádám', 'osztaly': '14SZF'},
+    {'nev': 'Kiss László', 'osztaly': '14SZF'},
+    {'nev': 'Németh Christopher', 'osztaly': '14SZF'},
+];
+
+function getosztaly(){
+
+    let output = '<table>';
+    output += `<tr><th>Név</th><th>Osztály</th></tr>`;
+    diakok.forEach((diak)  => {
+            output += `<tr><td>${diak.nev}</td><td>${diak.osztaly}</td></tr>`;
+    });
+    output += '</table>';
+    document.body.innerHTML = output;
+}
+
+getosztaly();
